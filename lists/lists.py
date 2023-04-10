84223e1780f9d4a292b6307f1d52af8c3658135d
Roman <roma2000kuba@gmail.com>
2023-04-10 23:52:05 +0300

    ex_5 done

--- lists/lists.py
+++ lists/lists.py
@@ -1,1 +1,9 @@
+nums = input("enter numbers:  ").split()
+nums = [int(num) for num in nums]
 
+positive_nums = []
+for num in nums:
+    if num > 0:
+        positive_nums.append(num)
+
+print("Positive numbers in the list: ", positive_nums)
