7bfb2308a8002e4855df1a16c7d9946496f00eeb
Roman <roma2000kuba@gmail.com>
2023-04-10 23:57:50 +0300

    ex_7  done

--- lists/lists.py
+++ lists/lists.py
@@ -1,1 +1,10 @@
+nums = input("enter numbers: ").split()
+nums = [int(num) for num in nums]
 
+min_index = nums.index(min(nums))
+max_index = nums.index(max(nums))
+
+nums[min_index], nums[max_index] = nums[max_index], nums[min_index]
+
+print("List with changed places of minimum and maximum elements:")
+print(nums)
