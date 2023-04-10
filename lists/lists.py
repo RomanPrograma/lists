280cd8f0167f49318410867df3007d06e36900d4
Roman <roma2000kuba@gmail.com>
2023-04-10 23:38:54 +0300

    ex_1 done

--- lists/lists.py
+++ lists/lists.py
@@ -1,1 +1,17 @@
+import random
+list=[]
+for i in range(10):
+    list.append(random.randint(1, 100))
+print(list)
 
+even_count = 0
+odd_count = 0
+
+for num in list:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Кількість парних чисел:", even_count)
+print("Кількість непарних чисел:", odd_count)
