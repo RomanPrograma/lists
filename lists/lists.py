43f29baff7d47d567ed7519acb18e6ef778fee9e
Roman <roma2000kuba@gmail.com>
2023-04-10 23:59:03 +0300

    ex_9 done

--- lists/lists.py
+++ lists/lists.py
@@ -1,1 +1,11 @@
+nums = input("enter numbers: ").split()
+nums = [int(num) for num in nums]
 
+average = sum(nums) / len(nums)
+
+count = 0
+for num in nums:
+    if num > average:
+        count += 1
+
+print("The number of numbers greater than the arithmetic mean:", count)
