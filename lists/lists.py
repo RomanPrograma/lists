886499067917f25a3d6c193f4b281e539f97ca2d
Roman <roma2000kuba@gmail.com>
2023-04-10 10:11:14 +0300

    ex_2 done

--- lists/lists.py
+++ lists/lists.py
@@ -1,1 +1,29 @@
 
+import random
+list = []
+for i in range (13):
+    list.append(random.randint(-10, 10))
+print(list)
+
+for i in list:
+    if  i %2==0:
+       
+        print('pair',i )
+print()
+odd=0
+for i in list:
+    if  i%2 != 0:
+        odd=i
+        print('odd', odd)
+print()
+neg=0
+for i in list:
+    if i<0:
+        neg=i
+        print('neg', neg)
+print()
+pos=0
+for i in list:
+    if i>0:
+        pos=i
+        print('pos', pos)
